d916868acd7e6ee7905b08d6d15bd136264e32ca
Oraka Emmanuel <oraka.emmanuel@gmail.com>
2023-07-19 08:23:47 -0700

    added

--- app/providers/ModalProvider.tsx
+++ app/providers/ModalProvider.tsx
@@ -1,8 +1,8 @@
 "use client";
 import React from "react";
-import RegisterModal from "../components/modals/RegisterModal";
-import LoginModal from "../components/modals/LoginModal";
-import AdminModal from "../components/modals/AdminModal";
+import RegisterModal from "@/components/modals/RegisterModal";
+import LoginModal from "@/components/modals/LoginModal";
+import AdminModal from "@/components/modals/AdminModal";
 
 const ModalProvider = () => {
   return (
