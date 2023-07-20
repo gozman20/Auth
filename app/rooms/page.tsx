7b1f2218dd1de0264ffe3d2e983045b93c2a12ed
Oraka Emmanuel <oraka.emmanuel@gmail.com>
2023-07-20 13:35:23 +0100

    added

--- app/rooms/page.tsx
+++ app/rooms/page.tsx
@@ -1,38 +1,31 @@
-// import React from "react";
-// import RoomSearch from "../components/rooms/RoomSearch";
-// import RoomCard from "../components/rooms/RoomCard";
-// import styles from "../components/styles";
-// import { getRooms } from "../actions/getRooms";
-// import { RoomParams } from "../actions/getRooms";
-// import ClientOnly from "../components/ClientOnly";
-
-// interface Homeprops {
-//   searchParams: RoomParams;
-// }
-// const Rooms = async ({ searchParams }: Homeprops) => {
-//   const rooms = await getRooms(searchParams);
-
-//   return (
-//     <ClientOnly>
-//       <div className="pt-[145px]">
-//         <RoomSearch />
-//         <div className={`${styles.paddingX}`}>
-//           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 pt-[30px] gap-4">
-//             {rooms.map((room) => (
-//               <RoomCard key={room.id} room={room} />
-//             ))}
-//           </div>
-//         </div>
-//       </div>
-//     </ClientOnly>
-//   );
-// };
-
-// export default Rooms;
 import React from "react";
+import RoomSearch from "@/components/rooms/RoomSearch";
+import RoomCard from "@/components/rooms/RoomCard";
+import styles from "@/components/styles";
+import getRooms from "@/actions/getRooms";
+import { RoomParams } from "@/actions/getRooms";
+import ClientOnly from "@/components/ClientOnly";
 
-const page = () => {
-  return <div></div>;
+interface Homeprops {
+  searchParams: RoomParams;
+}
+const Rooms = async ({ searchParams }: Homeprops) => {
+  const rooms = await getRooms(searchParams);
+
+  return (
+    <ClientOnly>
+      <div className="pt-[145px]">
+        <RoomSearch />
+        <div className={`${styles.paddingX}`}>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 pt-[30px] gap-4">
+            {rooms.map((room) => (
+              <RoomCard key={room.id} room={room} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </ClientOnly>
+  );
 };
 
-export default page;
+export default Rooms;
